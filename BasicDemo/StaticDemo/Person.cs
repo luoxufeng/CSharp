f3b10724187e6f878f655573d6edabb831c48705
unknown <xfluo@DST57191.cn1.global.ctrip.com>
2014-03-22 12:45:37 +0800

    add solusiton

---
+++ BasicDemo/StaticDemo/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticDemo
+{
+    class Person
+    {
+        private static string name;
+
+        public string GetName()
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "哈哈，我是来测试静态变量的哦";
+            }
+            return name;
+        }
+    }
+}
---
+++ BasicDemo/StaticDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = new Person().GetName();
+            Console.WriteLine("哈哈，我的值是多少了?=="+name);
+
+            Console.Write("我的值为:"+new Person().GetName());
+            Console.Read();
+        }
+    }
+}
